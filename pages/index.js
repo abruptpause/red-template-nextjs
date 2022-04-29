9aa473e97c42a3b116827100bc434918b61e11c2
ixc7 <isaacxpreston7@gmail.com>
2022-04-28 23:19:50 -0400

    scrollable section in mockup (2)

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,30 @@
 
 const Home = () => (
   <main>
-
-    <div>
-
-      <div className='absolute top-0 left-0 grid grid-cols-3 w-full' style={{ backgroundColor: 'rgba(255, 0, 0, 0.4)', height: '200vh' }}>
+    <div className="relative">
+      <div className='absolute top-0 left-0 grid grid-cols-3 w-full h-full' style={{ backgroundColor: 'rgba(255, 0, 0, 0.4)' }}>
         <div className='bg-green-200 col-span-1 col-start-3'>
           <div className='top-0 right-0 sticky flex bg-teal-600 items-center justify-center'>
-            <h1 className='text-center text-3xl capitalize'>sticky element</h1>
+            <h1 className='text-center text-3xl capitalize'>sticky content</h1>
           </div>
         </div>
       </div>
 
       <section className='flex items-center justify-center min-h-screen bg-purple-400'>
-        <h1 className='text-center text-5xl capitalize'>section nil</h1>
+        <h1 className='text-center text-5xl capitalize'>section 1</h1>
       </section>
 
       <section className='flex items-center justify-center min-h-screen bg-purple-700'>
-        <h1 className='text-center text-5xl capitalize'>section one</h1>
+        <h1 className='text-center text-5xl capitalize'>section 2</h1>
       </section>
-
     </div>
 
-
     <section className='relative flex items-center justify-center min-h-screen bg-purple-200'>
-      <h1 className='text-center text-5xl capitalize'>section two</h1>
+      <h1 className='text-center text-5xl capitalize'>section 3</h1>
     </section>
 
     <section className='relative flex items-center justify-center min-h-screen bg-blue-500'>
-      <h1 className='text-center text-5xl capitalize'>section three</h1>
+      <h1 className='text-center text-5xl capitalize'>section 4</h1>
     </section>
   </main>
 )
