141f44b73090cbe10417775bad1a1aa7995bb755
ixc7 <isaacxpreston7@gmail.com>
2022-04-28 21:59:24 -0400

    better simple component example

--- components/simple.js
+++ components/simple.js
@@ -3,7 +3,7 @@
 // https://codepen.io/anon/pen/JOOBxg
 
 const Simple = () => (
-  <div className="grid w-full bg-blue-400 min-h-screen items-center justify-center p-20">
+  <div className="grid w-full bg-blue-400 min-h-screen justify-end">
     <div id="child">
     </div>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
 import Simple from '../components/simple.js'
-import Table from '../components/table.js'
-import Text from '../components/text.js'
+// import Table from '../components/table.js'
+// import Text from '../components/text.js'
 
 const Home = () => (
   <div>
     <Simple />
-    <Table />
-    <Text />
+    <div className="min-h-screen bg-purple-200"></div>
   </div>
 )
 
