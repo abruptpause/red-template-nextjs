6a0fff16e0389046595300d62f4dd9927f166c84
ixc7 <isaacxpreston7@gmail.com>
2022-04-28 23:09:28 -0400

    inner and outer sections in mockup work w/ scroll

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,35 @@
 #!/usr/bin/env node
 
-// import Simple from '../components/simple.js'
-// import Table from '../components/table.js'
-// import Text from '../components/text.js'
-
 const Home = () => (
   <main>
 
-    <section className="grid grid-cols-3 bg-cyan-700 h-screen" label="Hero Section">
+    <div>
 
-      <div className="relative grid grid-cols-8 items-start bg-green-200 col-span-1 col-start-3">
-
-        <h1 className="absolute bottom-0 right-0 w-full text-center bg-teal-100 p-10 text-lg capitalize">sticky container</h1>
-
-        <div className="top-0 right-0 sticky flex bg-teal-600 col-span-6 col-start-2 aspect-square items-center justify-center">
-          <h1 className="text-center text-3xl capitalize">sticky element</h1>
+      <div className='absolute top-0 left-0 grid grid-cols-3 w-full' style={{ backgroundColor: 'rgba(255, 0, 0, 0.4)', height: '200vh' }}>
+        <div className='bg-green-200 col-span-1 col-start-3'>
+          <div className='top-0 right-0 sticky flex bg-teal-600 items-center justify-center'>
+            <h1 className='text-center text-3xl capitalize'>sticky element</h1>
+          </div>
         </div>
-
       </div>
 
+      <section className='flex items-center justify-center min-h-screen bg-purple-400'>
+        <h1 className='text-center text-5xl capitalize'>section nil</h1>
+      </section>
+
+      <section className='flex items-center justify-center min-h-screen bg-purple-700'>
+        <h1 className='text-center text-5xl capitalize'>section one</h1>
+      </section>
+
+    </div>
+
+
+    <section className='relative flex items-center justify-center min-h-screen bg-purple-200'>
+      <h1 className='text-center text-5xl capitalize'>section two</h1>
     </section>
 
-    <section className=" flex items-center justify-center min-h-screen bg-purple-200">
-      <h1 className="text-center text-5xl capitalize">section one</h1>
-    </section>
-
-
-    <section className=" flex items-center justify-center min-h-screen bg-blue-500">
-      <h1 className="text-center text-5xl capitalize">section two</h1>
+    <section className='relative flex items-center justify-center min-h-screen bg-blue-500'>
+      <h1 className='text-center text-5xl capitalize'>section three</h1>
     </section>
   </main>
 )
