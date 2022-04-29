a468406e59ed3d0d0c27d67ffbee5a6168b7c712
ixc7 <isaacxpreston7@gmail.com>
2022-04-28 22:41:39 -0400

    add lorem util, build out sticky component mockup

--- components/simple.js
+++ components/simple.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
+import { lorem } from '../lib/util.js'
+
 // https://codepen.io/anon/pen/JOOBxg
 
+// child element inherits the parent height if:
+// - parent is changed from `grid` to `flex`
+// - aspect-square` is removed
+
 const Simple = () => (
-  <div className="grid w-full bg-blue-400 min-h-screen justify-end">
-    <div id="child">
+  <div className="grid grid-rows-5 bg-blue-700 h-screen">
+    <div className="grid grid-rows-3 items-center bg-green-200 col-span-2 col-start-2">
+      <div className="bg-teal-600 col-span-1 col-start-2 " />
     </div>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env node
 
-import Simple from '../components/simple.js'
+// import Simple from '../components/simple.js'
 // import Table from '../components/table.js'
 // import Text from '../components/text.js'
 
 const Home = () => (
-  <div>
-    <Simple />
-    <div className="min-h-screen bg-purple-200"></div>
-  </div>
+  <main>
+
+    <section className="grid grid-cols-3 bg-cyan-700 h-screen" label="Hero Section">
+
+      <div className="relative grid grid-cols-8 items-start bg-green-200 col-span-1 col-start-3">
+
+        <h1 className="absolute bottom-0 right-0 w-full text-center bg-teal-100 p-10 text-lg capitalize">sticky container</h1>
+
+        <div className="top-0 right-0 sticky flex bg-teal-600 col-span-6 col-start-2 aspect-square items-center justify-center">
+          <h1 className="text-center text-3xl capitalize">sticky element</h1>
+        </div>
+
+      </div>
+
+    </section>
+
+    <section className=" flex items-center justify-center min-h-screen bg-purple-200">
+      <h1 className="text-center text-5xl capitalize">section one</h1>
+    </section>
+
+
+    <section className=" flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-center text-5xl capitalize">section two</h1>
+    </section>
+  </main>
 )
 
 export default Home
