44b3de5383349856baf29d902340590839a00ab8
ixc7 <isaacxpreston7@gmail.com>
2022-05-01 18:24:02 -0400

    css object/style cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
 #!/usr/bin/env node
 
-const StickyImage = () => (
-  <div className="grid justify-center w-full items-center aspect-square bg-red-200 text-red-700 text-5xl text-center my-24">
+const css = {
+  StickyImage: 'grid justify-center w-full items-center aspect-square bg-red-200 text-red-700 text-5xl text-center my-24',
+  Sticky: {
+    section: 'absolute top-0 left-0 grid grid-cols-3 w-full h-full bg-red-600/[.08]',
+    div: 'sticky overflow-y-scroll col-span-1 col-start-3 h-screen top-0 right-0 bg-red-400 p-16'
+  },
+  PlaceHolder: {
+    section: 'flex items-center justify-center min-h-screen',
+    h1: 'text-center text-5xl capitalize'
+  }
+}
+
+const Header = ({ text = ['HEADER', 'HEADER', 'HEADER'] }) => (
+  <div className='text-7xl flex col-span-2 justify-center items-center sticky h-screen top-0 right-0'>
     <div>
-      <h1>Image</h1>
+      {
+        Array.isArray(text) && text.length
+        ? text.map((line, key) => (<h1 key={key}>{line}</h1>))
+        : null
+      }
+    </div>
+  </div>
+)
+
+const StickyImage = ({ text }) => (
+  <div className={css.StickyImage}>
+    <div>
+      {
+        text
+        ? <h1>Image</h1>
+        : null
+      }
     </div>
   </div>
 )
 
 const Sticky = () => (
-  <section className='absolute top-0 left-0 grid grid-cols-3 w-full h-full bg-red-600/[.08]'>
-      <div className='overflow-y-scroll col-span-1 col-start-3 h-screen top-0 right-0 sticky bg-red-400 p-16'>
+  <section className={css.Sticky.section}>
+      <Header />
+      <div className={css.Sticky.div}>
         <StickyImage />
         <StickyImage />
         <StickyImage />
@@ -18,9 +47,13 @@ const Sticky = () => (
   </section>
 )
 
-const PlaceHolder = ({ bg = 'bg-slate-200', fg = 'text-slate-700' }) => (
-  <section className={`flex items-center justify-center min-h-screen ${bg}`}>
-    <h1 className={`text-center text-5xl capitalize ${fg}`}>Section</h1>
+const PlaceHolder = ({ bg = 'slate-200', fg = 'slate-700', text }) => (
+  <section className={`${css.PlaceHolder.section} bg-${bg}`}>
+  {
+    text
+    ? <h1 className={`${css.PlaceHolder.h1} text-${fg}`}>{text}</h1>
+    : null
+   }
   </section>
 )
 
@@ -28,12 +61,12 @@ const Home = () => (
   <main>
     <section className='relative'>
       <Sticky />
-      <PlaceHolder bg='bg-blue-200' fg='text-blue-700' />
-      <PlaceHolder bg='bg-blue-400' fg='text-blue-700' />
+      <PlaceHolder bg='blue-200' fg='blue-700' />
+      <PlaceHolder bg='blue-400' fg='blue-700' />
     </section>
-    <PlaceHolder bg='bg-teal-200' fg='text-teal-700' />
-    <PlaceHolder bg='bg-teal-500' fg='text-teal-700' />
-    <PlaceHolder bg='bg-teal-800' fg='text-teal-700' />
+    <PlaceHolder bg='teal-200' fg='teal-700' />
+    <PlaceHolder bg='teal-500' fg='teal-700' />
+    <PlaceHolder bg='teal-800' fg='teal-700' />
   </main>
 )
 
