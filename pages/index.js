8b645a7d6b1ea88fba3a97b897c3ac81ea145f08
ixc7 <isaacxpreston7@gmail.com>
2022-05-01 16:58:51 -0400

    right nav images and scroll

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 #!/usr/bin/env node
 
+
+const StickyImage = () => (
+
+
+  <div className="grid justify-center w-full items-center aspect-square bg-blue-300 text-slate-600 text-5xl text-center my-16">
+    <div>
+      <h1>Image</h1>
+    </div>
+  </div>
+)
+
+const Sticky = () => (
+  <section className='absolute top-0 left-0 grid grid-cols-3 w-full h-full bg-red-600/[.08]'>
+      <div className='overflow-y-scroll col-span-1 col-start-3 h-screen top-0 right-0 sticky bg-teal-600 p-16'>
+        <StickyImage />
+        <StickyImage />
+        <StickyImage />
+      </div>
+  </section>
+)
+
+
 const PlaceHolder = ({ bg = 'bg-slate-50', text = 'section' }) => (
   <section className={`flex items-center justify-center min-h-screen ${bg}`}>
     <h1 className='text-center text-5xl capitalize'>{text}</h1>
   </section>
 )
-
-const Sticky = ({ bg = 'bg-sky-500/[.01]', text = 'sticky' }) => (
-  <section className={ `absolute top-0 left-0 grid grid-cols-3 w-full h-full ${bg}` }>
-    <div className='bg-green-200 col-span-1 col-start-3'>
-      <div className='h-screen top-0 right-0 sticky flex bg-teal-600 items-center justify-center p-10'>
-        <p className='text-center text-4xl capitalize'>{text}</p>
-      </div>
-    </div>
-  </section>
-)
-
 const Home = () => (
   <main>
     <section className='relative'>
