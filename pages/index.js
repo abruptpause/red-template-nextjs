a3d5ea70def82d1027aeac432841b0d2d54dcf66
ixc7 <isaacxpreston7@gmail.com>
2022-05-02 12:00:35 -0400

    label components, use freightNeo font class

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const css = {
-  Header: 'sticky flex h-screen top-0 right-0 text-center text-blue-800 text-8xl col-span-2 justify-center items-center',
+  Header: 'freight-neo sticky flex h-screen top-0 right-0 text-center text-blue-700 text-9xl col-span-2 justify-center items-center',
   Square: 'grid justify-center w-full items-center aspect-square bg-red-200 text-red-700 text-5xl text-center my-24',
   Sticky: {
     section: 'absolute top-0 left-0 grid grid-cols-3 w-full h-full bg-red-600/[.08]',
@@ -9,18 +9,18 @@ const css = {
   },
   PlaceHolder: {
     section: 'flex items-center justify-center min-h-screen',
-    h1: 'text-center text-5xl capitalize'
+    h1: 'freight-neo text-center text-5xl'
   }
 }
 
-const Header = ({ text = ['HEADER', 'TEXT'] }) => (
+const Header = ({ text = ['left', 'hero', 'section'] }) => (
   <div className={css.Header}>
     <div>
-    {
-      Array.isArray(text) && text.length
-      ? text.map((line, key) => (<h1 key={key}>{line}</h1>))
-      : null
-    }
+      {
+        Array.isArray(text) && text.length
+        ? text.map((line, key) => (<h1 key={key}>{line}</h1>))
+        : null
+      }
     </div>
   </div>
 )
@@ -28,11 +28,12 @@ const Header = ({ text = ['HEADER', 'TEXT'] }) => (
 const Square = ({ text }) => (
   <div className={css.Square}>
     <div>
-    {
-      text
-      ? <h1>Image</h1>
-      : null
-    }
+      <h1 className='freight-neo text-center text-red-300  text-3xl'>Media</h1>
+      {
+        text
+        ? <h1>Image</h1>
+        : null
+      }
     </div>
   </div>
 )
@@ -41,6 +42,7 @@ const Sticky = () => (
   <section className={css.Sticky.section}>
     <Header />
     <div className={css.Sticky.div}>
+      <h1 className='absolute freight-neo text-center text-red-500 top-0 left-0 w-full my-16 text-3xl'>right sticky</h1>
       <Square />
       <Square />
       <Square />
@@ -48,13 +50,13 @@ const Sticky = () => (
   </section>
 )
 
-const PlaceHolder = ({ bg = 'bg-slate-200', fg = 'text-slate-700', text }) => (
+const PlaceHolder = ({ bg = 'bg-slate-100', fg = 'text-slate-300', text }) => (
   <section className={`${css.PlaceHolder.section} ${bg}`}>
-  {
-    text
-    ? <h1 className={`${css.PlaceHolder.h1} ${fg}`}>{text}</h1>
-    : null
-  }
+    {
+      text
+      ? <h1 className={`${css.PlaceHolder.h1} ${fg}`}>{text}</h1>
+      : null
+    }
   </section>
 )
 
@@ -62,14 +64,13 @@ const Home = () => (
   <main>
     <section className='relative'>
       <Sticky />
-      <PlaceHolder bg='bg-blue-200' />
-      <PlaceHolder bg='bg-blue-400' />
+      <PlaceHolder bg='bg-blue-200' fg='text-blue-300' text='underlay 1' />
+      <PlaceHolder bg='bg-blue-400' fg='text-blue-500' text='underlay 2' />
     </section>
 
-    <PlaceHolder bg='bg-teal-200' />
-    <PlaceHolder bg='bg-teal-500' />
-    <PlaceHolder bg='bg-teal-800' />
-
+    <PlaceHolder bg='bg-teal-200' fg='text-teal-300' text='section 1' />
+    <PlaceHolder bg='bg-teal-400' fg='text-teal-500' text='section 2' />
+    <PlaceHolder bg='bg-teal-600' fg='text-teal-700' text='section 3' />
   </main>
 )
 
