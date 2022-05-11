cda3c439865ce1a7f22c9006d7391704835f57f6
ixc7 <isaacxpreston7@gmail.com>
2022-05-10 20:31:16 -0400

    display images on page

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,16 @@ const Header = ({ text = ['hero', 'title', 'text'], caption = 'left sticky' }) =
   )
 }
 
-const Square = ({ text }) => {
+const Square = ({ text, img }) => {
   const css = {
     div: 'relative grid justify-center w-full items-center aspect-square bg-red-200 text-red-700 text-5xl text-center my-24 scroll-element',
     h1: 'freight-neo text-center text-red-300 text-2xl'
   }
 
   return (
-    <div className={css.div}>
+    <div className={css.div} style={{
+        backgroundImage: `url('${img}')`
+      }}>
       <div>
         <h1 className={css.h1}>Media</h1>
         {
@@ -44,7 +46,7 @@ const Square = ({ text }) => {
 // the header and the right bar.
 const Sticky = () => {
   const css = {
-    section: 'absolute top-0 left-0 grid grid-cols-3 w-full h-full bg-red-600/[.08]',
+    section: 'absolute top-0 left-0 grid grid-cols-3 w-full h-full',
     div: 'sticky overflow-y-scroll col-span-1 col-start-3 h-screen top-0 bg-red-400 p-16 scroll-container scroll-pt-16',
     h1: 'absolute freight-neo text-center text-red-500 top-0 left-0 w-full py-16 text-3xl scroll-element'
   }
@@ -52,49 +54,50 @@ const Sticky = () => {
   return (
     <section className={css.section}>
       <Header text={['featuring', 'art', 'by', 'john ivy']} caption='abrupt pause' />
+
       <div className={css.div}>
         <h1 className={css.h1}>right sticky</h1>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        <Square img='/images/side/SIDE1.png' />
+        <Square img='/images/side/SIDE2.png' />
+        <Square img='/images/side/SIDE3.png' />
       </div>
     </section>
   )
 }
 
-const PlaceHolder = ({ bg = 'bg-slate-100', fg = 'text-slate-200', text }) => {
+const PlaceHolder = ({ bg = 'bg-slate-100', fg = 'text-slate-200', img, text }) => {
   const css = {
-    section: 'flex items-center justify-center min-h-screen',
-    h1: 'freight-neo text-center text-5xl'
+    section: `flex items-center justify-center min-h-screen bg-center bg-cover ${bg}`,
+    h1: `freight-neo text-center text-5xl ${fg}`
   }
 
   return (
-    <section className={`${css.section} ${bg}`}>
+    <section
+      className={css.section}
+      style={{
+        backgroundImage: `url('${img}')`
+      }}
+    >
       {
       text
-        ? <h1 className={`${css.h1} ${fg}`}>{text}</h1>
+        ? <h1 className={css.h1}>{text}</h1>
         : null
     }
     </section>
   )
 }
 
-// so the sticky is 200vh
-// and the header is span-2 and the right section is span-1
-// so now we just have to transition the header component from span-2 to span-3 then boom good
 const Home = () => (
   <main>
     <section className='relative'>
       <Sticky />
-      <PlaceHolder bg='bg-blue-200' fg='text-blue-300' text='underlay 1' />
-      <PlaceHolder bg='bg-blue-400' fg='text-blue-500' text='underlay 2' />
+      <PlaceHolder bg='bg-blue-200' fg='text-blue-300' img='/images/main/hero.jpg' />
+      <PlaceHolder bg='bg-white' />
     </section>
 
-    <PlaceHolder bg='bg-teal-200' fg='text-teal-300' text='section 1' />
-    <PlaceHolder bg='bg-teal-400' fg='text-teal-500' text='section 2' />
-    <PlaceHolder bg='bg-teal-600' fg='text-teal-700' text='section 3' />
+    <PlaceHolder bg='bg-teal-200' fg='text-teal-300' img='/images/main/JIV1.jpg' />
+    <PlaceHolder bg='bg-teal-400' fg='text-teal-500' img='/images/main/JIV4.jpg' />
+    <PlaceHolder bg='bg-teal-600' fg='text-teal-700' img='/images/main/JIV5.jpg' />
   </main>
 )
 
