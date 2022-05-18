feb510d17338daccbd8f0471ab3f6eb5ae958b62
ixc7 <isaacxpreston7@gmail.com>
2022-05-18 18:09:48 -0400

    add first, second, third... placeholders

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-// TODO
-// NeueHaasGrotesk
-
 const Header = ({ text = ['hero', 'title', 'text'], caption = 'left sticky' }) => {
   const div = 'freight-neo sticky flex h-screen top-0 text-center text-white text-9xl col-span-2 justify-center items-center'
   const h1 = 'text-xl pb-12'
@@ -21,7 +18,7 @@ const Header = ({ text = ['hero', 'title', 'text'], caption = 'left sticky' }) =
   )
 }
 
-const Square = ({ text, img, start }) => {
+const Square = ({ img, start }) => {
   const div = `bg-cover bg-center relative grid justify-center w-full items-center aspect-square bg-white my-8 scroll-element col-span-7 ${start}`
 
   return (
@@ -56,7 +53,6 @@ const Sticky = () => {
 const PlaceHolder = ({ bg = 'bg-white', fg = 'text-slate-200', img, text }) => {
   const section = `flex items-center justify-center min-h-screen bg-center bg-cover ${bg}`
   const h1 = `freight-neo text-center text-5xl ${fg}`
-
   return (
     <section
       className={section}
@@ -73,70 +69,84 @@ const PlaceHolder = ({ bg = 'bg-white', fg = 'text-slate-200', img, text }) => {
   )
 }
 
-const Home = () => (
-  <main>
-    <section className='relative'>
-      <Sticky />
-      <PlaceHolder img='/images/main/hero.jpg' />
-      <PlaceHolder bg='bg-slate-200' />
-    </section>
+// col-span-8
+// col-span-10 col-start-2
+const Section = ({ content, bg = 'bg-slate-100' }) => (
+  <div className={`min-h-screen grid grid-cols-12 ${bg}`}>
+    <div className='col-span-8 grid grid-cols-12 gap-14 m-14'>
+      {content}
+    </div>
+  </div>
+)
 
-    <section className='min-h-screen grid grid-cols-3 bg-slate-100'>
-      <div className='col-span-2 grid grid-cols-12 gap-14 m-14'>
-      <div
-        className='col-span-8 bg-center bg-cover aspect-5/7 self-start'
-        style={{
-          backgroundImage: 'url(\'/images/main/JIV1.jpg\')'
-        }}
-      >
-      </div>
-      <div
-        className='col-span-4 bg-center bg-cover aspect-4/5 self-end mb-24'
-        style={{
-          backgroundImage: 'url(\'/images/main/JIV2.jpg\')'
-        }}
-      >
-      </div>
-      </div>
-    </section>
+const First = (
+  <>
+    <div
+      className='col-span-8 bg-center bg-cover aspect-5/7 self-start'
+      style={{
+        backgroundImage: `url('/images/main/JIV1.jpg')`
+      }}
+    >
+    </div>
+    <div
+      className='col-span-4 bg-center bg-cover aspect-4/5 self-end mb-24'
+      style={{
+        backgroundImage: `url('/images/main/JIV2.jpg')`
+      }}
+    >
+    </div>
+  </>
+)
 
-    <section className='min-h-screen grid grid-cols-3 bg-slate-200'>
-      <div className='col-span-2 grid grid-cols-12 gap-14 m-14'>
-      <div
-        className='col-span-6 bg-center bg-cover aspect-2/3 self-start'
-        style={{
-          backgroundImage: 'url(\'/images/main/JIV3.jpg\')'
-        }}
-      >
-      </div>
-      <div className='col-span-6 self-end font-extralight text-2xl'>
-        <h1 className='freight-neo mb-14 font-normal text-3xl text-slate-400'>Pterodactyl-543</h1>
+const Second = (
+  <>
+    <div
+      className='col-span-6 bg-center bg-cover aspect-2/3 self-start'
+      style={{
+        backgroundImage: `url('/images/main/JIV3.jpg')`
+      }}
+    >
+    </div>
+    <div className='col-span-6 self-end font-extralight text-2xl'>
+        <h1 className='freight-neo mb-14 font-normal text-3xl text-slate-400'>{`Pterodactyl-543`}</h1>
         <p className='mb-4'>{`So, in not thinking too much about what I'm trying to do, and just doing it.`}</p>
         <p className='mb-4'>{`For me, I rarely approach a work with a preconception of how it should look. It's much too frustrating, and I'm just not that good.`}</p>
         <p className='mb-4'>{`My best laid plans are never realized, and often what I end up with has little or nothing to do with my initial premise.`}</p>
         <p className='mb-4'>{`It's not a very efficient way to work, I suppose.`}</p>
-      </div>
-      </div>
-    </section>
+    </div>
+  </>
+)
 
-    <section className='min-h-screen grid grid-cols-3 bg-slate-100'>
-        <div className='col-span-2 grid grid-cols-12 gap-14 m-14'>
-        <div
-          className='col-span-6 aspect-4/5 bg-center bg-cover self-start'
-          style={{
-            backgroundImage: 'url(\'/images/main/JIV4.jpg\')'
-          }}
-        >
-        </div>
-        <div
-          className='col-span-5 aspect-square bg-center bg-cover self-end'
-          style={{
-            backgroundImage: 'url(\'/images/main/JIV5.jpg\')'
-          }}
-        >
-        </div>
-        </div>
-    </section>
+const Third = (
+  <>
+    <div
+      className='col-span-6 aspect-4/5 bg-center bg-cover self-start'
+      style={{
+        backgroundImage: 'url(\'/images/main/JIV4.jpg\')'
+      }}
+    >
+    </div>
+    <div
+      className='col-span-5 aspect-square bg-center bg-cover self-end'
+      style={{
+        backgroundImage: 'url(\'/images/main/JIV5.jpg\')'
+      }}
+    >
+    </div>
+  </>
+)
+
+const Home = () => (
+  <main>
+    <div className='relative'>
+      <Sticky />
+      <PlaceHolder img='/images/main/hero.jpg' />
+      <PlaceHolder bg='bg-slate-200' />
+    </div>
+
+    <Section content={First} />
+    <Section content={Second} bg='bg-slate-200' />
+    <Section content={Third} />
 
   </main>
 )
