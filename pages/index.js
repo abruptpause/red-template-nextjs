4a66a13ab07324ca460c946b454ed77683d4018b
ixc7 <isaacxpreston7@gmail.com>
2022-05-18 18:36:12 -0400

    semantics/formatting

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const Header = ({ text = ['hero', 'title', 'text'], caption = 'left sticky' }) => {
+const BigText = ({ text = ['big', 'text'], subtext = 'subtext' }) => {
   return (
     <div className={`
       freight-neo sticky flex h-screen top-0 text-center text-white
       text-9xl col-span-2 justify-center items-center
     `}>
       <div>
-        <h1 className='text-xl pb-12'>{caption}</h1>
+        <h1 className='text-xl pb-12'>{subtext}</h1>
         {
           Array.isArray(text) && text.length
             ? text.map((line, key) => (<h1 key={key}>{line}</h1>))
@@ -18,7 +18,7 @@ const Header = ({ text = ['hero', 'title', 'text'], caption = 'left sticky' }) =
   )
 }
 
-const Square = ({ img, start }) => {
+const RightSquare = ({ img, start }) => {
   return (
     <div
     className={`
@@ -32,11 +32,12 @@ const Square = ({ img, start }) => {
   )
 }
 
-// the header and the right bar.
-const Sticky = () => {
+const RightBar = () => {
   return (
     <section className='absolute top-0 left-0 grid grid-cols-3 w-full h-full'>
-      <Header text={['featuring', 'art', 'by', 'john ivy']} caption='abrupt pause' />
+
+      <BigText text={['featuring', 'art', 'by', 'john ivy']} subtext='abrupt pause' />
+
       <div className={`
         sticky overflow-y-scroll col-span-1 col-start-3 h-screen top-0
         scroll-container bg-slate-300 grid grid-cols-12 scroll-py-20
@@ -44,15 +45,16 @@ const Sticky = () => {
         <h1 className={`
           col-span-12 text-white freight-neo text-center top-0 left-0 text-3xl pt-6 mb-8 sticky
         `}>featured work</h1>
-        <Square img='/images/side/SIDE1.png' start='col-start-5' />
-        <Square img='/images/side/SIDE2.png' start='col-start-2' />
-        <Square img='/images/side/SIDE3.png' start='col-start-5' />
+        <RightSquare img='/images/side/SIDE1.png' start='col-start-5' />
+        <RightSquare img='/images/side/SIDE2.png' start='col-start-2' />
+        <RightSquare img='/images/side/SIDE3.png' start='col-start-5' />
       </div>
+
     </section>
   )
 }
 
-const PlaceHolder = ({ bg = 'bg-white', fg = 'text-slate-200', img, text }) => {
+const EmptySection = ({ bg = 'bg-white', fg = 'text-slate-200', img, text }) => {
   return (
     <section
       className={`flex items-center justify-center min-h-screen bg-center bg-cover ${bg}`}
@@ -79,8 +81,15 @@ const Section = ({ children, bg = 'bg-slate-100' }) => (
   </div>
 )
 
-const First = (
-  <>
+const Home = () => (
+  <main>
+    <div className='relative'>
+      <RightBar />
+      <EmptySection img='/images/main/hero.jpg' />
+      <EmptySection bg='bg-slate-200' />
+    </div>
+
+    <Section>
     <div
       className='col-span-8 bg-center bg-cover aspect-5/7 self-start'
       style={{
@@ -95,11 +104,9 @@ const First = (
       }}
     >
     </div>
-  </>
-)
+    </Section>
 
-const Second = (
-  <>
+    <Section bg='bg-slate-200'>
     <div
       className='col-span-6 bg-center bg-cover aspect-2/3 self-start'
       style={{
@@ -109,51 +116,37 @@ const Second = (
     </div>
     <div className='col-span-6 self-end font-extralight text-2xl'>
         <h1 className='freight-neo mb-14 font-normal text-3xl text-slate-400'>{`Pterodactyl-543`}</h1>
-        <p className='mb-4'>{`So, in not thinking too much about what I'm trying to do, and just doing it.`}</p>
-        <p className='mb-4'>{`For me, I rarely approach a work with a preconception of how it should look. It's much too frustrating, and I'm just not that good.`}</p>
-        <p className='mb-4'>{`My best laid plans are never realized, and often what I end up with has little or nothing to do with my initial premise.`}</p>
-        <p className='mb-4'>{`It's not a very efficient way to work, I suppose.`}</p>
+        <p className='mb-4'>
+          {`So, in not thinking too much about what I'm trying to do, and just doing it.`}
+        </p>
+        <p className='mb-4'>
+          {`For me, I rarely approach a work with a preconception of how it should look. It's much too frustrating, and I'm just not that good.`}
+        </p>
+        <p className='mb-4'>
+          {`My best laid plans are never realized, and often what I end up with has little or nothing to do with my initial premise.`}
+        </p>
+        <p className='mb-4'>
+          {`It's not a very efficient way to work, I suppose.`}
+        </p>
     </div>
-  </>
-)
 
-const Third = (
-  <>
+    </Section>
+
+    <Section>
     <div
       className='col-span-6 aspect-4/5 bg-center bg-cover self-start'
       style={{
-        backgroundImage: 'url(\'/images/main/JIV4.jpg\')'
+        backgroundImage: `url('/images/main/JIV4.jpg')`
       }}
     >
     </div>
     <div
       className='col-span-5 aspect-square bg-center bg-cover self-end'
       style={{
-        backgroundImage: 'url(\'/images/main/JIV5.jpg\')'
+        backgroundImage: `url('/images/main/JIV5.jpg')`
       }}
     >
     </div>
-  </>
-)
-
-const Home = () => (
-  <main>
-    <div className='relative'>
-      <Sticky />
-      <PlaceHolder img='/images/main/hero.jpg' />
-      <PlaceHolder bg='bg-slate-200' />
-    </div>
-
-    <Section>
-      {First}
-    </Section>
-
-    <Section bg='bg-slate-200'>
-      {Second}
-    </Section>
-
-    <Section>
-      {Third}
     </Section>
   </main>
 )
