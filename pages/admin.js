d3150d8ba627a7a81f3bf59e46460a410e406f79
ixc7 <isaacxpreston7@gmail.com>
2022-06-17 20:55:13 -0400

    re add git-gateway

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-/* eslint-disable */
-
 // https://github.com/netlify/netlify-cms/issues/3580
 // https://www.luckymedia.dev/blog/page-builder-with-netlify-cms-and-next-js
 
@@ -32,13 +30,13 @@ const config = {
 
   // https://stackoverflow.com/questions/62203427/problem-with-configuring-netlify-cms-with-git-gateway
   // Backend configuration, in this case with git
-  // backend: {
-    // name: 'git-gateway',
-    // branch: 'main',
-    // repo: 'abruptpause/red-template-nextjs',
+  backend: {
+    name: 'git-gateway',
+    branch: 'main',
+    repo: 'abruptpause/red-template-nextjs',
     // branch: 'master',
-    // squash_merges: true
-  // },
+    squash_merges: true
+  },
 
   // Local backend is used during development
   local_backend: true,
