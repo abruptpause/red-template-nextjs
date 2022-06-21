ef7d35aa5163e07ce19515a619cd5e6f86c18120
ixc7 <isaacxpreston7@gmail.com>
2022-06-21 19:26:22 -0400

    change CMS fields

--- pages/admin.js
+++ pages/admin.js
@@ -8,6 +8,7 @@ import Script from 'next/script'
 // https://github.com/netlify/netlify-cms/issues/3580
 // https://www.luckymedia.dev/blog/page-builder-with-netlify-cms-and-next-js
 // https://stackoverflow.com/questions/62203427/problem-with-configuring-netlify-cms-with-git-gateway
+// https://answers.netlify.com/t/fetch-data-from-the-cms-in-create-react-app/40084/3
 
 const config = {
   // We want to manually init the config file
@@ -18,11 +19,11 @@ const config = {
     name: 'git-gateway',
     branch: 'main',
     repo: 'abruptpause/red-template-nextjs',
-    squash_merges: true
+    squash_merges: true,
   },
 
   // Local backend is used during development
-  local_backend: true,
+  // local_backend: true,
 
   // Where to store the images
   media_folder: 'public/images/',
@@ -30,30 +31,39 @@ const config = {
   // The Pages collection
   collections: [
     {
-      name: 'Pages',
-      label: 'Page',
-      editor: { preview: false },
-      label_singular: 'Page',
-      folder: 'content/pages',
+      name: 'Images',
+      label: 'Image',
+      // editor: { preview: false },
+      label_singular: 'Image',
+      folder: 'public/Images',
       create: true,
-      slug: '{{slug}}',
-      extension: 'md',
-      format: 'yaml-frontmatter',
+      // slug: '{{slug}}',
+      slug: '{{title}}_{{slug}}',
+      // extension: 'md',
+      extension: 'json',
+      // format: 'yaml-frontmatter',
+      format: 'json',
       fields: [
         {
-          label: 'Title',
-          name: 'title',
+          label: 'Description',
+          name: 'description',
           widget: 'string',
-          required: true
-        }
-      ]
-    }
-  ]
+          required: false,
+        },
+        {
+          label: 'File',
+          name: 'file',
+          widget: 'image',
+          required: true,
+        },
+      ],
+    },
+  ],
 }
 
 const CMS = dynamic(
   () =>
-    import('netlify-cms-app').then((cms) => {
+    import('netlify-cms-app').then(cms => {
       cms.init({ config })
     }),
   {
@@ -62,7 +72,7 @@ const CMS = dynamic(
       <div>
         <p>Loading...</p>
       </div>
-    )
+    ),
   }
 )
 
